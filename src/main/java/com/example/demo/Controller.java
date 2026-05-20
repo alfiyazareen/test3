2f497343907db01bb29206683457000dadcc8127
alfiyazareen <alfiyazareen01@gmail.com>
2026-05-20 23:54:34 +0530

    first commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/hi")
     public String hi(){
-        return "hi from string";
+        return "hi from string";//
     }
 
 }
